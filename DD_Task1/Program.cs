30dddb7340b5c263c7c413626bbee513443bf12c
smesh <smesh@LAPTOP-MH9GRGGB>
2023-05-26 15:03:31 +0300

    Добавьте файлы проекта.

---
+++ DD_Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using searchingLibrary;
+using System.Reflection;
+
+public static class Task_1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter path to book");
+        string pathToBook = Console.ReadLine();
+        string pathToUniqueWords = "C:\\Users\\smesh\\Desktop\\proga\\c#\\DD_Task1\\DD_Task1\\uniqueWords.txt";
+
+        try
+        {
+            cleverSearchClass search = new cleverSearchClass();
+
+            var type = typeof(cleverSearchClass);
+            MethodInfo methodInfo = type.GetMethod("analyser", BindingFlags.Instance | BindingFlags.NonPublic);
+            object reflectionReturn = methodInfo.Invoke(search, new object[] {pathToBook});
+            Dictionary<string, int> totalDictionary = (Dictionary<string, int>)reflectionReturn;
+   
+            StreamWriter uniqeWords = new StreamWriter(pathToUniqueWords);
+            foreach (var word in totalDictionary.OrderByDescending(x => x.Value))
+                uniqeWords.WriteLine($"{word.Key}\t {word.Value}");
+
+            uniqeWords.Close();
+        
+        }
+
+        catch { Console.WriteLine("Возникло исключение!"); }
+    }
+}
---
+++ searchingLibrary/cleverSearchClass.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace searchingLibrary
+{
+    public class cleverSearchClass
+    {
+
+        private Dictionary<string, int> analyser(string pathToBook)
+        {
+            var specialWords = new Dictionary<string, int>();
+            string[] textWords;
+
+            char[] charsToTrim = {' ', '.', ',', '-', '\n', '\"', '(', ')', ':', ';', '[', ']', '\t', '!', '?',
+                                                             '0','1','2','3','4','5','6','7','8','9'};
+            string[] notUnique = {"я", "мы","он","она","они","оно","ты","вы","нее","у","в","него","ее","его","мне","чтобы","себя", "тебя",
+                                      "не", "с","к","все","за","это","от","по","так","то","из","бы","ему","ей","была","будто","быть", "тут", "уж",
+                                      "о","и","или","в","на","под","над","а","но","как","что", "сноска", "же", "этот","ним","ваше", "свое", "даже", "всё",
+                                      "было","были","был","еще","для","когда","вот","ни","ну","до","нет", "да", "этого", "про", "эти","этих","кто",
+                                      "того","вас","тем","себе","ли","вам","чем","которые","во","свою","перед", "сам", "мой", "меня", "чтоб", "если",
+                                      "их","только","уже","очень","всех","при","со","чтото","своего","им","без", "ней", "там", "го", "т","г","м", "ж"};
+
+            StreamReader bookLines = new StreamReader(pathToBook);
+
+            while (true)
+            {
+                string allLines = bookLines.ReadLine();
+                if (allLines == null)
+                    break;
+                else
+                    allLines = allLines.ToLower();
+
+                textWords = allLines.Split(' ');
+
+                foreach (string word in textWords)
+                {
+                    string cleanWord = "";
+
+                    foreach (char letter in word)
+                        cleanWord += letter.ToString().Trim(charsToTrim);
+                    
+                    if (specialWords.ContainsKey(cleanWord))
+                        specialWords[cleanWord]++;
+                    else
+                        specialWords[cleanWord] = 1;
+                    
+                }
+
+            }
+            bookLines.Close();
+
+            specialWords.Remove("");
+
+            foreach (string cleanWord in notUnique)
+                specialWords.Remove(cleanWord);
+
+            return specialWords;
+        }
+    }
+}
