8f50e502f8e5b268296d30e87bb2d10871fc9d62
Smeshook <126953768+Smeshook@users.noreply.github.com>
2023-05-29 14:45:03 +0300

    Update cleverSearchClass.cs

--- searchingLibrary/cleverSearchClass.cs
+++ searchingLibrary/cleverSearchClass.cs
@@ -4,39 +4,34 @@ namespace searchingLibrary
 {
     public class cleverSearchClass
     {
-
-        private Dictionary<string, int> analyser(string fileText)
-        {
-            var specialWords = new Dictionary<string, int>();
-
-            char[] charsToTrim = {' ', '.', ',', '-', '\r','\n', '\"', '(', ')', ':', ';', '[', ']', '\t', '!', '?',
-                                                             '0','1','2','3','4','5','6','7','8','9'};
-            string[] notUnique = {"я", "мы","он","она","они","оно","ты","вы","нее","у","в","него","ее","его","мне","чтобы","себя", "тебя",
+        public string[] notUnique = {"я", "мы","он","она","они","оно","ты","вы","нее","у","в","него","ее","его","мне","чтобы","себя", "тебя",
                                       "не", "с","к","все","за","это","от","по","так","то","из","бы","ему","ей","была","будто","быть", "тут", "уж",
                                       "о","и","или","в","на","под","над","а","но","как","что", "сноска", "же", "этот","ним","ваше", "свое", "даже", "всё",
                                       "было","были","был","еще","для","когда","вот","ни","ну","до","нет", "да", "этого", "про", "эти","этих","кто",
                                       "того","вас","тем","себе","ли","вам","чем","которые","во","свою","перед", "сам", "мой", "меня", "чтоб", "если",
                                       "их","только","уже","очень","всех","при","со","чтото","своего","им","без", "ней", "там", "го", "т","г","м", "ж"};
 
-            string[] textWords = fileText.ToLower().Split(' ');
+        private Dictionary<string, int> analyser(string fileText)
+        {
+            var specialWords = new Dictionary<string, int>();
+            string tempStr = fileText.Replace("\r", "").Replace("\r\n","");
+            tempStr = new string((from c in fileText
+                                     where (char.IsLetter(c) || char.IsWhiteSpace(c)) && c!='\r' && c!='\n'
+                                     select c).ToArray());
+                        
+            string[] textWords = tempStr.ToLower().Split(' ');
             foreach (string word in textWords)
             {
-                string cleanWord = "";
-
-                foreach (char letter in word)
-                    cleanWord += letter.ToString().Trim(charsToTrim);
-                    
-                if (specialWords.ContainsKey(cleanWord))
-                    specialWords[cleanWord]++;
+                if (specialWords.ContainsKey(word))
+                    specialWords[word]++;
                 else
-                    specialWords[cleanWord] = 1;
-                    
+                    specialWords[word] = 1;                  
             }
 
             specialWords.Remove("");
 
-            foreach (string cleanWord in notUnique)
-                specialWords.Remove(cleanWord);
+            foreach (string word in notUnique)
+                specialWords.Remove(word);
 
             return specialWords;
         }
