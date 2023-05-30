e0f38c8e955535bde4c5ab0e21e667d2153baa1e
smesh <smesh@LAPTOP-MH9GRGGB>
2023-05-30 21:08:46 +0300

    erfg

--- DD_Task1/Program.cs
+++ DD_Task1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Concurrent;
 using System.IO;
 using searchingLibrary;
 using System.Reflection;
+using System.Diagnostics;
 
 public static class Task_1
 {
@@ -11,26 +12,57 @@ public static class Task_1
     {
         Console.WriteLine("Enter path to book");
         string pathToBook = Console.ReadLine();
-        string pathToUniqueWords = @".\uniqueWords.txt";
+        string pathToUniqueWordsPublic = @".\uniqueWordsPublic.txt";
+        string pathToUniqueWordsPrivate = @".\uniqueWordsPrivate.txt";
+
+        Stopwatch watchPrivate = new Stopwatch();
+        Stopwatch watchPublic = new Stopwatch();
 
         try
         {
             cleverSearchClass search = new cleverSearchClass();
 
             StreamReader fileText = new StreamReader(pathToBook);
+            string fileString = fileText.ReadToEnd();
 
             var type = typeof(cleverSearchClass);
-            MethodInfo methodInfo = type.GetMethod("analyser", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo methodInfo = type.GetMethod("privateAnalyser", BindingFlags.Instance | BindingFlags.NonPublic);
 
-            object reflectionReturn = methodInfo.Invoke(search, new object[] {fileText.ReadToEnd()});
-            Dictionary<string, int> totalDictionary = (Dictionary<string, int>)reflectionReturn;
+            object reflectionReturn = methodInfo.Invoke(search, new object[] {fileString});
 
-            StreamWriter uniqeWords = new StreamWriter(pathToUniqueWords);
-            foreach (var word in totalDictionary.OrderByDescending(x => x.Value))
-                uniqeWords.WriteLine($"{word.Key}\t {word.Value}");
+            watchPrivate.Start();
+            Dictionary<string, int> totalDictionaryPrivate = (Dictionary<string, int>)reflectionReturn;
+            watchPrivate.Stop();
 
-            uniqeWords.Close();
-        
+            watchPublic.Start();
+            Dictionary<string, int> totalDictionaryPublic = search.PublicAnalyser(fileString);
+            watchPublic.Stop();
+
+            StreamWriter uniqeWordsPrivate = new StreamWriter(pathToUniqueWordsPrivate);
+            foreach (var word in totalDictionaryPrivate.OrderByDescending(x => x.Value))
+                uniqeWordsPrivate.WriteLine($"{word.Key}\t {word.Value}");
+
+            uniqeWordsPrivate.Close();
+
+            StreamWriter uniqeWordsPublic = new StreamWriter(pathToUniqueWordsPublic);
+            foreach (var word in totalDictionaryPublic.OrderByDescending(x => x.Value))
+                uniqeWordsPublic.WriteLine($"{word.Key}\t {word.Value}");
+
+            uniqeWordsPublic.Close();
+
+            TimeSpan tsPublic = watchPublic.Elapsed;
+            TimeSpan tsPrivate = watchPrivate.Elapsed;
+
+            string elapsedTimePublic = String.Format("{0:00}:{1:00}:{2:000}",
+                                                    tsPublic.Seconds,
+                                                    tsPublic.Milliseconds, tsPublic.Microseconds);
+            string elapsedTimePrivate = String.Format("{0:00}:{1:00}:{2:000}",
+                                                    tsPrivate.Seconds,
+                                                    tsPrivate.Milliseconds, tsPrivate.Microseconds);
+
+            Console.WriteLine("RunTime Public: " + elapsedTimePublic
+                                +"\n"+ "RunTime Private: " + elapsedTimePrivate);
+
         }
 
         catch { Console.WriteLine("Возникло исключение!"); }
--- searchingLibrary/cleverSearchClass.cs
+++ searchingLibrary/cleverSearchClass.cs
@@ -1,44 +1,118 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
 
 namespace searchingLibrary
 {
     public class cleverSearchClass
     {
-
-        private Dictionary<string, int> analyser(string fileText)
-        {
-            var specialWords = new Dictionary<string, int>();
-
-            char[] charsToTrim = {' ', '.', ',', '-', '\r','\n', '\"', '(', ')', ':', ';', '[', ']', '\t', '!', '?',
-                                                             '0','1','2','3','4','5','6','7','8','9'};
-            string[] notUnique = {"я", "мы","он","она","они","оно","ты","вы","нее","у","в","него","ее","его","мне","чтобы","себя", "тебя",
+        public Dictionary<string, int> specialWords = new Dictionary<string, int>();
+        public string[] notUnique = {"я", "мы","он","она","они","оно","ты","вы","нее","у","в","него","ее","его","мне","чтобы","себя", "тебя",
                                       "не", "с","к","все","за","это","от","по","так","то","из","бы","ему","ей","была","будто","быть", "тут", "уж",
                                       "о","и","или","в","на","под","над","а","но","как","что", "сноска", "же", "этот","ним","ваше", "свое", "даже", "всё",
                                       "было","были","был","еще","для","когда","вот","ни","ну","до","нет", "да", "этого", "про", "эти","этих","кто",
                                       "того","вас","тем","себе","ли","вам","чем","которые","во","свою","перед", "сам", "мой", "меня", "чтоб", "если",
                                       "их","только","уже","очень","всех","при","со","чтото","своего","им","без", "ней", "там", "го", "т","г","м", "ж"};
 
-            string[] textWords = fileText.ToLower().Split(' ');
+        private Dictionary<string, int> privateAnalyser(string fileText)
+        {
+            var specialWords = new Dictionary<string, int>();
+            string tempStr = new string((from c in fileText
+                                     where (char.IsLetter(c) || char.IsWhiteSpace(c)) && c!='\r' && c!='\n'
+                                     select c).ToArray());
+                        
+            string[] textWords = tempStr.ToLower().Split(' ');
             foreach (string word in textWords)
             {
-                string cleanWord = "";
-
-                foreach (char letter in word)
-                    cleanWord += letter.ToString().Trim(charsToTrim);
-                    
-                if (specialWords.ContainsKey(cleanWord))
-                    specialWords[cleanWord]++;
+                if (specialWords.ContainsKey(word))
+                    specialWords[word]++;
                 else
-                    specialWords[cleanWord] = 1;
-                    
+                    specialWords[word] = 1;                  
             }
 
             specialWords.Remove("");
 
-            foreach (string cleanWord in notUnique)
-                specialWords.Remove(cleanWord);
+            foreach (string word in notUnique)
+                specialWords.Remove(word);
 
             return specialWords;
         }
+
+        public Dictionary<string, int> PublicAnalyser(string fileText)
+        {
+            int NUMBER_OF_PARTS = 4;
+
+            var specialWords = new Dictionary<string, int>();
+            string tempStr = new string((from c in fileText
+                                         where (char.IsLetter(c) || char.IsWhiteSpace(c)) && c != '\r' && c != '\n'
+                                         select c).ToArray());
+
+            string[] textWords = tempStr.ToLower().Split(' ');
+
+            int chunkSize = textWords.Length / NUMBER_OF_PARTS;
+            string[][] chunks = new string[NUMBER_OF_PARTS][];
+
+            for (int i = 0; i < NUMBER_OF_PARTS; i++)
+            {
+                chunks[i] = new string[chunkSize];
+                Array.Copy(textWords, i * chunkSize, chunks[i], 0, chunkSize);
+            }
+          
+            List<Task<Dictionary<string, int>>> tasks = new List<Task<Dictionary<string, int>>>();
+
+            for (int i = 0; i < NUMBER_OF_PARTS; i++)
+            {
+                int taskNumber = i; 
+                tasks.Add(Task.Factory.StartNew(() => tinyAnalyzer(chunks[taskNumber], taskNumber)));
+
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            foreach (var task in tasks)
+            {
+                foreach (var kvp in task.Result)
+                {
+                    if (result.ContainsKey(kvp.Key))
+                    {
+                        result[kvp.Key] += kvp.Value;
+                    }
+                    else
+                    {
+                        result.Add(kvp.Key, kvp.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public Dictionary<string, int> tinyAnalyzer(string[] textWords, int taskNumber)
+        {
+
+            Console.WriteLine($"Task {taskNumber} выполняется.");
+
+            var specialWords = new Dictionary<string, int>();
+            foreach (string word in textWords)
+            {
+                if (specialWords.ContainsKey(word))
+                    specialWords[word]++;
+                else
+                {
+                    specialWords[word] = 1;
+                }
+            }
+
+            specialWords.Remove("");
+
+            foreach (string word in notUnique)
+                specialWords.Remove(word);
+
+            return specialWords;
+        }
+
     }
 }
